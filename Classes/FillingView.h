5fbc2646761df01f13b79f59fd870db1b7d54f30
beepscore <support@beepscore.com>
2010-03-03 18:07:43 -0800

    Add FillingView

---
+++ Classes/FillingView.h
@@ -0,0 +1,16 @@
+//
+//  FillingView.h
+//  Filling
+//
+//  Created by Steve Baker on 3/3/10.
+//  Copyright 2010 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FillingView : UIView {
+
+}
+
+@end
